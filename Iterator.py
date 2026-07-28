1735f625dbffb898ec23620d7dd213f93f3c395c
twinklesaha29-del <twinklesaha29@gmail.com>
2026-07-28 10:10:04 +0530

    Add files via upload

---
+++ Iterator.py
@@ -0,0 +1,9 @@
+# Program to demonstrate an Iterator
+
+fruits = ["Apple", "Banana", "Mango"]
+
+fruit_iterator = iter(fruits)
+
+print(next(fruit_iterator))
+print(next(fruit_iterator))
+print(next(fruit_iterator))
